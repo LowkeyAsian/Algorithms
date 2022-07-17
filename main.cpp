df4e7815a6a8d32f789bd5529ba972d46f336339
Fuad <93775671+LowkeyAsian@users.noreply.github.com>
2022-07-17 19:26:25 +0600

    MCM

--- main.cpp
+++ main.cpp
@@ -1,42 +1,60 @@
-#include <iostream>
+#include<iostream>
+#include<limits.h>
 
 using namespace std;
 
+int MatrixChainMultiplication(int p[], int n)
+{
+    int m[n][n];
+    int i, j, k, L, q;
+
+    for (i=1; i<n; i++)
+        m[i][i] = 0;
+
+
+    for (L=2; L<n; L++)
+    {
+        for (i=1; i<n-L+1; i++)
+        {
+            j = i+L-1;
+            m[i][j] = INT_MAX;  //assigning to maximum value
+
+            for (k=i; k<=j-1; k++)
+            {
+                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if (q < m[i][j])
+                {
+                    m[i][j] = q;    //if number of multiplications found less that number will be updated.
+                }
+            }
+        }
+    }
+
+    return m[1][n-1];   //returning the final answer which is M[1][n]
+
+}
 
 int main()
 {
-   int row, col, m1[10][10], m2[10][10], sum[10][10];
-   cout<<"Enter the number of rows(should be >1 and <10): ";
-   cin>>row;
+    int n,i;
+    cout<<"Enter number of matrices\n";
+    cin>>n;
 
-   cout<<"Enter the number of column(should be >1 and <10): ";
-   cin>>col;
-   cout << "Enter the elements of first matrix(enter first row first): ";
-   for (int i = 0;i<row;i++ ) {
-     for (int j = 0;j < col;j++ ) {
-       cin>>m1[i][j];
-     }
-   }
-   cout << "Enter the elements of second matrix(enter first row first): ";
-   for (int i = 0;i<row;i++ ) {
-     for (int j = 0;j<col;j++ ) {
-       cin>>m2[i][j];
-     }
-   }
-   for (int i = 0;i<row;i++ ) {
-      for (int j = 0;j<col;j++ ) {
-        sum[i][j]=m1[i][j]+m2[i][j];
-      }
-   }
-      cout<<"Output: ";
-      cout<<endl;
-      for(int i = 0; i < row; ++i)
-        for(int j = 0; j < col; ++j)
-        {
-            cout << sum[i][j] << "  ";
-            if(j == col - 1)
-            cout << endl;
-        }
-   return 0;
+    n++;
+
+    int arr[n];
+
+    cout<<"Enter dimensions \n";
+
+    for(i=0;i<n;i++)
+    {
+        cout<<"Enter d"<<i<<" :: ";
+        cin>>arr[i];
+    }
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Minimum number of multiplications is "<<MatrixChainMultiplication(arr, size);
+
+    return 0;
 }
-
