10c0ac5eb783a5ff60c6116186965c8c7fc3d13a
Fuad <93775671+LowkeyAsian@users.noreply.github.com>
2022-07-17 19:26:50 +0600

    merge sort

--- main.cpp
+++ main.cpp
@@ -1,60 +1,80 @@
-#include<iostream>
-#include<limits.h>
-
+#include <iostream>
 using namespace std;
 
-int MatrixChainMultiplication(int p[], int n)
+void merge(int array[], int const left, int const mid, int const right)
 {
-    int m[n][n];
-    int i, j, k, L, q;
-
-    for (i=1; i<n; i++)
-        m[i][i] = 0;
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
 
 
-    for (L=2; L<n; L++)
-    {
-        for (i=1; i<n-L+1; i++)
-        {
-            j = i+L-1;
-            m[i][j] = INT_MAX;  //assigning to maximum value
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
 
-            for (k=i; k<=j-1; k++)
-            {
-                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
-                if (q < m[i][j])
-                {
-                    m[i][j] = q;    //if number of multiplications found less that number will be updated.
-                }
-            }
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,
+        indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
         }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
     }
 
-    return m[1][n-1];   //returning the final answer which is M[1][n]
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
 
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+void printArray(int A[], int size)
+{
+    for (auto i = 0; i < size; i++)
+        cout << A[i] << " ";
 }
 
 int main()
 {
-    int n,i;
-    cout<<"Enter number of matrices\n";
-    cin>>n;
+    int arr[] = { 92, 11, 54, 5, 1, 7 };
+    auto arr_size = sizeof(arr) / sizeof(arr[0]);
 
-    n++;
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
 
-    int arr[n];
+    mergeSort(arr, 0, arr_size - 1);
 
-    cout<<"Enter dimensions \n";
-
-    for(i=0;i<n;i++)
-    {
-        cout<<"Enter d"<<i<<" :: ";
-        cin>>arr[i];
-    }
-
-    int size = sizeof(arr)/sizeof(arr[0]);
-
-    cout<<"Minimum number of multiplications is "<<MatrixChainMultiplication(arr, size);
-
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
     return 0;
 }
