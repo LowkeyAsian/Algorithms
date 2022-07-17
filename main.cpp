f71255720d0bd9c35b5802ff7b897f15c42ac4eb
Fuad <93775671+LowkeyAsian@users.noreply.github.com>
2022-07-17 19:05:55 +0600

    Bubble sort

--- main.cpp
+++ main.cpp
@@ -1,63 +1,38 @@
 #include<iostream>
+
 using namespace std;
 
-int k=0;
-
-void sort_func(int A[],int B[],int n)
-{
-    int count[k+1],t;
-    for(int i=0;i<=k;i++)
-    {
-
-        count[i] = 0;
-    }
-    for(int i=0;i<n;i++)
-    {
-
-        t = A[i];
-        count[t]++;
-    }
-    for(int i=1;i<=k;i++)
-    {
-
-        count[i] = count[i]+count[i-1];
-    }
-    for(int i=0;i<n;i++)
-    {
-
-        t = A[i];
-        B[count[t]] = t;
-
-        count[t]=count[t]-1;
-    }
-}
 int main()
+
 {
-    int n;
-    cout<<"Enter the size of the array :";
-    cin>>n;
 
+ int n, i, arr[50], j, temp;
 
-    int A[n],B[n];
-    cout<<"Enter the array elements: "<<endl;
-    for(int i=0;i<n;i++)
+     cout<<"Enter the Size (max. 25): ";
+     cin>>n;
+     cout<<"Enter "<<n<<" Numbers: ";
+
+ for(i=0; i<n; i++)
+     cin>>arr[i];
+     cout<<"\nSorting Array using Bubble Sort \n";
+ for(i=0; i<(n-1); i++)
     {
-        cin>>A[i];
-        if(A[i]>k)
+ for(j=0; j<(n-i-1); j++)
         {
-
-            k = A[i];
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
     }
+     cout<<"\nArray Sorted Successfully!\n";
+     cout<<"\nThe New Array is: \n";
 
-    sort_func(A,B,n);
+for(i=0; i<n; i++)
+     cout<<arr[i]<<" ";
+     cout<<endl;
 
-
-    for(int i=1;i<=n;i++)
-    {
-        cout<<B[i]<<" ";
-    }
-
-    cout<<"\n";
     return 0;
 }
