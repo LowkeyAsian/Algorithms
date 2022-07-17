d016dd5928413f288413793ccc32c0fc114e39db
Fuad <93775671+LowkeyAsian@users.noreply.github.com>
2022-07-17 19:27:48 +0600

    quick sort

--- main.cpp
+++ main.cpp
@@ -1,38 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-int deno[] = { 1, 2, 5, 10, 20,
-               50, 100, 500, 1000 };
-int n = sizeof(deno) / sizeof(deno[0]);
-
-void findMin(int V)
+void swap(int* a, int* b)
 {
-    sort(deno, deno + n);
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
 
-    vector<int> ans;
+    for (int j = low; j <= high- 1; j++)
+    {
 
-    for (int i = n - 1; i >= 0; i--) {
-
-
-        while (V >= deno[i]) {
-            V -= deno[i];
-            ans.push_back(deno[i]);
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
         }
     }
-
-
-    for (int i = 0; i < ans.size(); i++)
-        cout << ans[i] << " ";
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 
 
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+void displayArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout<<arr[i]<<"\t";
+
+}
+
 int main()
 {
-    int n = 79;
-    cout << "Following is minimal"
-         << " number of change for " << n
-         << ": ";
-    findMin(n);
+    int arr[] = {12,23,3,43,51,35,19,45};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Input array"<<endl;
+    displayArray(arr,n);
+    cout<<endl;
+    quickSort(arr, 0, n-1);
+    cout<<"Array sorted with quick sort"<<endl;
+    displayArray(arr,n);
     return 0;
 }
